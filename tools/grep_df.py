4fd3f9d44898dec4c05f6c95b3c77fd130ce6335
Mark Young <mark.francis.young@gmail.com>
2024-03-12 15:48:07 -0500

    lint fix

--- tools/grep_df.py
+++ tools/grep_df.py
@@ -1,7 +1,8 @@
+import pdb
 import sys
 
-import tabulate
 import pandas as pd
+
 
 def find_value(df, value):
     for col in df.columns:
@@ -9,13 +10,15 @@ def find_value(df, value):
         if (count := idx.sum()) > 0:
             results = df[idx][col].value_counts()
             print(f'------\n{col} shows {count} {value}\n{results}')
-                
+
+
 def do(input_file):
     df = pd.read_csv(input_file, dtype=str)
-    
+
     find_value(df, 'Oodle')
 
-    import pdb; pdb.set_trace()
+    pdb.set_trace()
+
 
 if __name__ == '__main__':
     do(sys.argv[1])
